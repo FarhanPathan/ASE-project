abc274b82d43d5ca9e09de2cadf9d599e098f3d6
FarhanPathan <farhankhanpathan43@gmail.com>
2023-11-12 06:49:40 +0000

    Unit testing of Reset command

---
+++ ASE_PROJECT_UNITTEST/ResetUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASE_PROJECT_UNITTEST
+{
+    [TestClass]
+    public class ResetUnitTest
+    {
+        [TestMethod]
+        public void ValidSyntex()
+        {
+            // Arrange
+            var reset = new Reset();
+            string[] commandParts = { "RESET" };
+
+            // Act
+            bool result = reset.SyntaxCheck(commandParts, false);
+
+            // Assert
+            Assert.IsTrue(result, "Valid syntax should return true.");
+        }
+
+        [TestMethod]
+        public void InvalidArgument()
+        {
+            // Arrange
+            var reset = new Reset();
+            string[] commandParts = { "RESET", "INVALID" }; // Invalid arguments
+
+            // Act
+            bool result = reset.SyntaxCheck(commandParts, false);
+
+            // Assert
+            Assert.IsFalse(result, "Invalid argument should return false.");
+        }
+    }
+}
